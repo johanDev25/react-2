992de3a8c7a20572fe3aa4387d787dca395f1c0d
Johan <johan225@hotmail.es>
2019-11-05 15:23:01 -0500

    Agrega funcionalidad de sumar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props){
+  	super(props);
+  	this.state = {
+  		val : 0
+  	}
+  }
+
+  render() { 	
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.val}</span>
+        <button id="inc" onClick={this.sumNum.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+
+  sumNum(){
+  	this.setState({
+      val: this.state.val + 1
+    });
   }
 }
 
